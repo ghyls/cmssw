ff2e946d523d387194ce88a37dd47faf445d0e8a
Mario Gonzalez <mario.gonzalez.carpintero@cern.ch>
2025-09-24 10:55:31 +0200

    Implemented a TrivialSerialiser plugin factory

--- TrivialSerialisation/Common/interface/TrivialSerialiser.h
+++ TrivialSerialisation/Common/interface/TrivialSerialiser.h
@@ -1,36 +1,68 @@
+#ifndef TrivialSerialisation_Common_TrivialSerialiser_h
+#define TrivialSerialisation_Common_TrivialSerialiser_h
 
-
-
+#include <cstdio>
+#include <concepts>
 #include "TrivialSerialisation/Common/interface/TrivialSerialiserBase.h"
 #include "TrivialSerialisation/Common/interface/TrivialCopyTraits.h"
+#include "DataFormats/Common/interface/Uninitialized.h"
 
+#include "FWCore/Framework/interface/WrapperBaseHandle.h"
 
-
-
-
-
-
-
+// defines all methods of TrivialSerialiserBase
 
 namespace ngt {
   template <typename T>
   class TrivialSerialiser : public TrivialSerialiserBase {
   public:
-    TrivialSerialiser() : TrivialSerialiserBase(), present(false) {}
+    TrivialSerialiser(int tag) : TrivialSerialiserBase(), present(false) {printf("%d\n", tag);}
+    // TrivialSerialiser() : TrivialSerialiserBase(), present(false) {}
+    // TODO: Where is present set to true?
 
-    bool hasTrivialCopyTraits_() const override;
-    bool hasTrivialCopyProperties_() const override;
-    void trivialCopyInitialize_(edm::AnyBuffer const& args) override;
-    edm::AnyBuffer trivialCopyParameters_() const override;
-    std::vector<std::span<const std::byte>> trivialCopyRegions_() const override;
-    std::vector<std::span<std::byte>> trivialCopyRegions_() override;
-    void trivialCopyFinalize_() override;
+    bool hasTrivialCopyTraits() const override;
+    bool hasTrivialCopyProperties() const override;
+    void trivialCopyInitialize(edm::WrapperBase& wrapper, edm::AnyBuffer const& args) override;
+    edm::AnyBuffer trivialCopyParameters(edm::WrapperBase const& wrapper) const override;
+    std::vector<std::span<const std::byte>> trivialCopyRegions(edm::WrapperBase const& wrapper) const override;
+    std::vector<std::span<std::byte>> trivialCopyRegions(edm::WrapperBase& wrapper) override;
+    void trivialCopyFinalize(edm::WrapperBase& wrapper) override;
+
+  private:
+    constexpr T construct_() {
+      if constexpr (requires { T(); }) {
+        return T();
+      } else {
+        return T(edm::kUninitialized);
+      }
+    }
+
+    const T& getWrappedObj_(edm::WrapperBase const& wrapper) const {
+      auto& w = static_cast<const edm::Wrapper<T>&>(wrapper);
+      if (not w.isPresent()) {
+        throw edm::Exception(edm::errors::LogicError) << "Attempt to access an empty Wrapper";
+      }
+      return *w.product();
+    }
+
+    T& getWrappedObj_(edm::WrapperBase& wrapper) {
+      auto& w = static_cast<edm::Wrapper<T>&>(wrapper);
+      if (not w.isPresent()) {
+        throw edm::Exception(edm::errors::LogicError) << "Attempt to access an empty Wrapper";
+      }
+      return *w.bareProduct();
+    }
 
 
+
+  private:
+    // TODO: KEEP IT CONST DO CONST_CAST ONLY ON THE NON CONST VERSION OF TCR
+    // T* obj = nullptr;
+    bool present;
+    // edm::WrapperBase *wrapper;
   };
 
   template <typename T>
-  inline bool Wrapper<T>::hasTrivialCopyTraits_() const {
+  inline bool TrivialSerialiser<T>::hasTrivialCopyTraits() const {
     if constexpr (requires(T& t) { edm::TrivialCopyTraits<T>::regions(t); }) {
       return true;
     }
@@ -38,17 +70,26 @@ namespace ngt {
   }
 
   template <typename T>
-  inline bool Wrapper<T>::hasTrivialCopyProperties_() const {
+  inline bool TrivialSerialiser<T>::hasTrivialCopyProperties() const {
     if constexpr (requires { typename edm::TrivialCopyTraits<T>::Properties; }) {
+      printf("TrivialCopyTraits<%s>::Properties is defined\n", edm::typeDemangle(typeid(T).name()).c_str());
       return true;
     }
     return false;
   }
 
   template <typename T>
-  void Wrapper<T>::trivialCopyInitialize_([[maybe_unused]] edm::AnyBuffer const& args) {
-    if (not present) {
-      throw edm::Exception(edm::errors::LogicError) << "Attempt to access an empty Wrapper";
+  // requires (!std::is_const_v<T>)
+  // void TrivialSerialiser<T>::trivialCopyInitialize([[maybe_unused]] edm::AnyBuffer const& args) {
+  void TrivialSerialiser<T>::trivialCopyInitialize(edm::WrapperBase& wrapper, edm::AnyBuffer const& args) {
+    printf("In TrivialSerialiser::trivialCopyInitialize. Heloosa\n");
+    auto& w = dynamic_cast<edm::Wrapper<T>&>(wrapper);
+    T& obj = w.bareProduct();
+    // const T* obj = w.product();
+    present = true;
+
+    if (not wrapper.isPresent()) {
+      throw edm::Exception(edm::errors::LogicError) << "Attempt to access an empty TrivialSerialiser66";
     }
     // if edm::TrivialCopyTraits<T>::Properties is not defined, do not call initialize()
     if constexpr (not requires { typename edm::TrivialCopyTraits<T>::Properties; }) {
@@ -64,11 +105,15 @@ namespace ngt {
       }
   }
 
+
   template <typename T>
-  inline edm::AnyBuffer Wrapper<T>::trivialCopyParameters_() const {
-    if (not present) {
-      throw edm::Exception(edm::errors::LogicError) << "Attempt to access an empty Wrapper";
-    }
+  inline edm::AnyBuffer TrivialSerialiser<T>::trivialCopyParameters(edm::WrapperBase const& wrapper) const {
+
+    // auto& w = dynamic_cast<edm::Wrapper<T> const&>(wrapper);
+    // if (not w.isPresent()) {
+    //   throw edm::Exception(edm::errors::LogicError) << "Attempt to access an empty Wrapper";
+    // }
+    const T& obj = getWrappedObj_(wrapper);
     // if edm::TrivialCopyTraits<T>::Properties is not defined, do not call properties()
     if constexpr (not requires { typename edm::TrivialCopyTraits<T>::Properties; }) {
       return {};
@@ -85,10 +130,10 @@ namespace ngt {
   }
 
   template <typename T>
-  inline std::vector<std::span<const std::byte>> Wrapper<T>::trivialCopyRegions_() const {
-    if (not present) {
-      throw edm::Exception(edm::errors::LogicError) << "Attempt to access an empty Wrapper";
-    }
+  inline std::vector<std::span<const std::byte>> TrivialSerialiser<T>::trivialCopyRegions(edm::WrapperBase const& wrapper) const {
+
+    const T& obj = getWrappedObj_(wrapper);
+
     if constexpr (requires(T const& t) { edm::TrivialCopyTraits<T>::regions(t); }) {
       return edm::TrivialCopyTraits<T>::regions(obj);
     } else {
@@ -100,11 +145,16 @@ namespace ngt {
   }
 
   template <typename T>
-  inline std::vector<std::span<std::byte>> Wrapper<T>::trivialCopyRegions_() {
-    if (not present) {
-      throw edm::Exception(edm::errors::LogicError) << "Attempt to access an empty Wrapper";
+  inline std::vector<std::span<std::byte>> TrivialSerialiser<T>::trivialCopyRegions(edm::WrapperBase& wrapper) {
+
+    auto& w = dynamic_cast<edm::Wrapper<T>&>(wrapper);
+    T& obj = w.bareProduct();
+
+    if (not w.isPresent()) {
+      throw edm::Exception(edm::errors::LogicError) << "Attempt to access an empty TrivialSerialiser";
     }
     if constexpr (requires(T& t) { edm::TrivialCopyTraits<T>::regions(t); }) {
+
       return edm::TrivialCopyTraits<T>::regions(obj);
     } else {
       throw edm::Exception(edm::errors::LogicError)
@@ -115,13 +165,16 @@ namespace ngt {
   }
 
   template <typename T>
-  inline void Wrapper<T>::trivialCopyFinalize_() {
-    if (not present) {
-      throw edm::Exception(edm::errors::LogicError) << "Attempt to access an empty Wrapper";
+  inline void TrivialSerialiser<T>::trivialCopyFinalize(edm::WrapperBase& wrapper) {
+    if (not wrapper.isPresent()) {
+      throw edm::Exception(edm::errors::LogicError) << "Attempt to access an empty TrivialSerialiser23";
     }
     if constexpr (requires(T& t) { edm::TrivialCopyTraits<T>::finalize(t); }) {
-      edm::TrivialCopyTraits<T>::finalize(obj);
+      auto& w = dynamic_cast<edm::Wrapper<T>&>(wrapper);
+      edm::TrivialCopyTraits<T>::finalize(*w.product());
     }
   }
 
 }  // namespace ngt
+
+#endif
--- TrivialSerialisation/Common/interface/TrivialSerialiserBase.h
+++ TrivialSerialisation/Common/interface/TrivialSerialiserBase.h
@@ -9,18 +9,19 @@
 #include <vector>
 
 namespace ngt {
-
   class TrivialSerialiserBase {
   public:
     TrivialSerialiserBase() {};
 
-	virtual bool hasTrivialCopyTraits() const = 0;
+
+    virtual bool hasTrivialCopyTraits() const = 0;
     virtual bool hasTrivialCopyProperties() const = 0;
-    virtual void trivialCopyInitialize(edm::AnyBuffer const& args) = 0;
-    virtual edm::AnyBuffer trivialCopyParameters() const = 0;
-    virtual std::vector<std::span<const std::byte>> trivialCopyRegions() const = 0;
-    virtual std::vector<std::span<std::byte>> trivialCopyRegions() = 0;
-    virtual void trivialCopyFinalize() = 0;
+    virtual void trivialCopyInitialize(edm::WrapperBase& wrapper, edm::AnyBuffer const& args) = 0;
+    // virtual void trivialCopyInitialize(edm::WrapperBase const& wrapper, edm::AnyBuffer const& args) = 0;
+    virtual edm::AnyBuffer trivialCopyParameters(edm::WrapperBase const& wrapper) const = 0;
+    virtual std::vector<std::span<const std::byte>> trivialCopyRegions(edm::WrapperBase const& wrapper) const = 0;
+    virtual std::vector<std::span<std::byte>> trivialCopyRegions(edm::WrapperBase& wrapper) = 0;
+    virtual void trivialCopyFinalize(edm::WrapperBase& wrapper) = 0;
 
     virtual ~TrivialSerialiserBase() = default;
   };
