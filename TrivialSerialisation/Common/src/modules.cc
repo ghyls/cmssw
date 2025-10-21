c23ccb86dd78a919f83faecf67624225fa13342b
Mario Gonzalez <mario.gonzalez.carpintero@cern.ch>
2025-10-21 15:14:23 +0200

    Removed all plugins from this file

--- TrivialSerialisation/Common/src/modules.cc
+++
@@ -1,8 +0,0 @@
-
-#include "TrivialSerialisation/Common/interface/TrivialSerialiserSourceFactory.h"
-#include "TrivialSerialisation/Common/interface/TrivialSerialiserSource.h"
-#include "TrivialSerialisation/Common/interface/TrivialSerialiser.h"
-#include "DataFormats/PortableTestObjects/interface/TestHostObject.h"
-#include "DataFormats/Portable/interface/PortableCollection.h"
-#include "DataFormats/PortableTestObjects/interface/TestSoA.h"
-#include "DataFormats/Provenance/interface/EventID.h"
